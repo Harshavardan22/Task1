9748d4e6f968eaf21f08a3fa0c8e43544d575e0f
Harshavardan22 <harsha4030764@gmail.com>
2025-04-23 22:05:06 +0530

    first commit

---
+++ Program1.java
@@ -0,0 +1,13 @@
+package com.guvi.task1;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=10;i<=50;i++){
+            System.out.println(" The number is :"+i);
+        }
+
+	}
+
+}
